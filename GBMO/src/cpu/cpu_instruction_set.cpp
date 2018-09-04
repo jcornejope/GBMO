e5feb2d4fca622c1125a9c77105b17aee6472e43
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2018-09-04 22:09:40 +0100

    Small change on _call_routine assert.

--- GBMO/src/cpu/cpu_instruction_set.cpp
+++ GBMO/src/cpu/cpu_instruction_set.cpp
@@ -1199,15 +1199,18 @@ u32 CPU::_reti()
 }
 u32 CPU::_call_routine( u8 const routine_address )
 {
-    assert( // RST Commands
-            routine_address == 0x00 || routine_address == 0x08 ||
-            routine_address == 0x10 || routine_address == 0x18 ||
-            routine_address == 0x20 || routine_address == 0x28 ||
-            routine_address == 0x30 || routine_address == 0x38 ||
-            // Interrupts
-            routine_address == 0x40 || routine_address == 0x48 ||
-            routine_address == 0x50 || routine_address == 0x58 ||
-            routine_address == 0x60 );
+    //assert( // RST Commands
+    //        routine_address == 0x00 || routine_address == 0x08 ||
+    //        routine_address == 0x10 || routine_address == 0x18 ||
+    //        routine_address == 0x20 || routine_address == 0x28 ||
+    //        routine_address == 0x30 || routine_address == 0x38 ||
+    //        // Interrupts
+    //        routine_address == 0x40 || routine_address == 0x48 ||
+    //        routine_address == 0x50 || routine_address == 0x58 ||
+    //        routine_address == 0x60 );
+
+    // Same but shorter
+    assert( routine_address <= 0x60 && ( routine_address & 0x07 ) == 0 );
 
     _push( m_registers.pc );
     m_registers.pc = routine_address;
