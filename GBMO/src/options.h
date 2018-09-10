add9ecd093f9be75b111b1a206dd1ed8e6046094
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2018-09-10 20:46:52 +0100

    Options minor change

--- GBMO/src/options.h
+++ GBMO/src/options.h
@@ -5,12 +5,16 @@
 
 struct Options
 {
-    Options() {}
+    Options() 
+        : m_resolution_scale ( 1 ) 
+        , m_volume( 0.f )
+        , m_log( true )
+    {}
 
     std::string m_rom_path;
+    std::string m_input_config_file_path;
 
-    u32 m_resolution_x;
-    u32 m_resolution_y;
+    u32 m_resolution_scale;
     float m_volume;
     bool m_log;
 };
