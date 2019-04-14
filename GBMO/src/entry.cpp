e3a914e5034344fbf5a2c9b770c6dd8a741ddbe1
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2019-04-14 17:09:26 +0100

    Clean the entry from rom paths

--- GBMO/src/entry.cpp
+++ GBMO/src/entry.cpp
@@ -16,18 +16,7 @@ int main( int argc, char* argv[] )
     Logger::create_instance( ".\\log.txt" );
 
     Options options;
-    options.m_rom_path = "D:\\Dev\\GBMO\\rom_b_cpu_instrs.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\01-special.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\02-interrupts.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\03-op sp,hl.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\04-op r,imm.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\05-op rp.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\06-ld r,r.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\07-jr,jp,call,ret,rst.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\08-misc instrs.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\09-op r,r.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\10-bit ops.gb";
-    //options.m_rom_path = "D:\\Dev\\GBMO\\Blargg-gb-tests\\cpu_instrs\\cpu_instrs\\individual\\11-op a,(hl).gb";
+    options.m_rom_path = "..\\..\\roms\\rom_b_cpu_instrs.gb";
 
     GBMO emulator(options);
 
