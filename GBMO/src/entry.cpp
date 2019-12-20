6a1293227d5e1f751f9376849373352edf0968a6
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2019-12-20 14:18:48 +0000

    Doesn't need to call flush on Logger anymore. Compil fix.

--- GBMO/src/entry.cpp
+++ GBMO/src/entry.cpp
@@ -33,7 +33,6 @@ int main( int argc, char* argv[] )
         while( emulator_running )
         {
             emulator_running = emulator.update();
-            Logger::instance().flush();
         }
 
         emulator.deinit();
