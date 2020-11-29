c59b4760b152f24d10b0ba605307eda50c8ed88b
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2020-11-29 15:42:06 +0000

    Some test code using 2 square channels in emulator.

--- GBMO/src/sound/sound.cpp
+++ GBMO/src/sound/sound.cpp
@@ -1,7 +1,10 @@
 
 #include "sound.h"
 
+#include "channels.h"
+
 #include "memory/memory_system.h"
+#include "memory/memory_span.h"
 #include "utils/assert.h"
 #include "utils/logger.h"
 #include "utils/utils.h"
@@ -19,6 +22,7 @@ Sound::Sound( MemorySystem& memory )
     : m_memory( memory )
     , m_sound_cycles( 0 )
     , m_buff_write_pos( 0 )
+    , m_sound_buffer( nullptr )
     , m_stream( nullptr )
     , m_device( 0 )
 {
@@ -33,7 +37,7 @@ bool Sound::init()
     desired.format = AUDIO_S16SYS;
     desired.channels = 2;
     desired.samples = AUDIO_BUFFER_SIZE / desired.channels;
-    desired.callback = audio_callback;
+    desired.callback = nullptr; // audio_callback;
     desired.userdata = this;
 
     m_device = SDL_OpenAudioDevice( NULL, 0, &desired, &obtained, 0 );
@@ -54,29 +58,79 @@ bool Sound::init()
 
     SDL_PauseAudioDevice( m_device, 0 ); // This means unpause!
 
+    m_sound_buffer = new sample_t[AUDIO_BUFFER_SIZE];
+    m_square1_channel = new SquareChannel( MemorySpan( m_memory, CH1_SWEEP_ADDR, 5 ) );
+    m_square2_channel = new SquareChannel( MemorySpan( m_memory, 0xFF15, 5 ) );
+
+    m_ch1_ms = new MemorySpan( m_memory, CH1_SWEEP_ADDR, 5 );
+
     return true;
 }
 
 void Sound::deinit()
 {
+    if( m_sound_buffer != nullptr )
+    {
+        delete[] m_sound_buffer;
+        m_sound_buffer = nullptr;
+    }
+
+    if( m_square1_channel != nullptr )
+    {
+        delete m_square1_channel;
+        m_square1_channel = nullptr;
+    }
+
+    if( m_square2_channel != nullptr )
+    {
+        delete m_square2_channel;
+        m_square2_channel = nullptr;
+    }
+    
     if( m_device != 0 )
         SDL_CloseAudioDevice( m_device );
+
+
+    delete m_ch1_ms;
 }
 
 void Sound::update( u32 cycles )
 {
     m_sound_cycles += cycles;
 
-    //if( SDL_AudioStreamAvailable( m_stream ) < AUDIO_BUFFER_SIZE * sizeof( sample_t ) )
-    //{
-    //    ADD NOISE FOR TESTING
-    //    sample_t buf[AUDIO_BUFFER_SIZE];
-    //    for( u32 i = 0; i < AUDIO_BUFFER_SIZE; ++i )
-    //        buf[i] = static_cast<sample_t>( rand() );
+    for( u32 i = 0; i < cycles; ++i )
+    {
+        // update oscillators
+        m_square1_channel->tick();
+        m_square2_channel->tick();
 
-    //    if( SDL_AudioStreamPut( m_stream, buf, AUDIO_BUFFER_SIZE * sizeof( sample_t ) ) )
-    //        LOG_E( LogCat::SOUND, "Failed to put samples into audio stream [%s]", SDL_GetError() );
-    //}
+        // update frame sequencer
+
+        // update mixer (ignoring Vin for now as it was never used).
+        ++m_mixer_cycles;
+        if( m_mixer_cycles > 95 )
+        {
+            m_mixer_cycles = 0;
+
+            sample_t sample = 0;
+            sample_t const sq1_out = static_cast<sample_t>( m_square1_channel->get_output() ) * 128;
+            SDL_MixAudioFormat( (u8*)&sample, (u8*)&sq1_out, AUDIO_S16SYS, sizeof( sample_t ), SDL_MIX_MAXVOLUME );
+            sample_t const sq2_out = static_cast<sample_t>( m_square2_channel->get_output() ) * 128;
+            SDL_MixAudioFormat( (u8*)&sample, (u8*)&sq2_out, AUDIO_S16SYS, sizeof( sample_t ), SDL_MIX_MAXVOLUME );
+
+            m_sound_buffer[m_buff_write_pos++] = sample;
+            m_sound_buffer[m_buff_write_pos++] = sample;
+
+            //m_sound_buffer[m_buff_write_pos++] = static_cast<sample_t>( rand() );
+            //m_sound_buffer[m_buff_write_pos++] = static_cast<sample_t>( rand() );
+
+            if( m_buff_write_pos == AUDIO_BUFFER_SIZE )
+            {
+                m_buff_write_pos = 0;
+                SDL_QueueAudio( m_device, m_sound_buffer, AUDIO_BUFFER_SIZE * sizeof( sample_t ) );
+            }
+        }
+    }
 }
 
 void Sound::enable()
@@ -85,6 +139,11 @@ void Sound::enable()
     // Frame sequencer is reset so that the next step will be 0
     // The square duty units are reset to the first step of the waveform
     // The wave channel's sample buffer is reset to 0.
+    if(m_square1_channel)
+        m_square1_channel->reset();
+
+    if( m_square2_channel )
+        m_square2_channel->reset();
 }
 
 void Sound::disable()
--- GBMO/src/sound/sound.h
+++ GBMO/src/sound/sound.h
@@ -5,6 +5,8 @@
 #include <SDL_audio.h>
 
 class MemorySystem;
+class SquareChannel;
+class MemorySpan;
 
 class Sound
 {
@@ -35,11 +37,23 @@ public:
     void fill_audio_buffer( Uint8* stream, int len );
 
 private:
+    SquareChannel* m_square1_channel;
+    SquareChannel* m_square2_channel;
+    //SquareOsc m_square1_channel;
+    //SquareOsc m_square2_channel;
+    //WaveOsc m_wave_channel;
+    //NoiseOsc m_noise_channel;
+    //SilenceOsc m_silence_channel;
+
     MemorySystem& m_memory;
+    MemorySpan* m_ch1_ms;
 
     u32 m_sound_cycles;
-    s32 m_buff_write_pos;
+    u32 m_buff_write_pos;
 
+    u32 m_mixer_cycles;
+
+    sample_t* m_sound_buffer;
     SDL_AudioStream* m_stream;
     SDL_AudioDeviceID m_device;
 };
