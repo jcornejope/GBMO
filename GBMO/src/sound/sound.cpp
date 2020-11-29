6b690e1c40765667f881a07df241c7bd86963f7b
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2020-11-29 16:27:31 +0000

    Added left/right output split. SO volume channel control on each SO. Removed unused code.

--- GBMO/src/sound/sound.cpp
+++ GBMO/src/sound/sound.cpp
@@ -12,11 +12,11 @@
 #include <SDL.h>
 #include <functional>
 
-void audio_callback( void* userdata, Uint8* stream, int len )
-{
-    Sound* sound = reinterpret_cast<Sound*>( userdata );
-    sound->fill_audio_buffer( stream, len );
-}
+//void audio_callback( void* userdata, Uint8* stream, int len )
+//{
+//    Sound* sound = reinterpret_cast<Sound*>( userdata );
+//    sound->fill_audio_buffer( stream, len );
+//}
 
 Sound::Sound( MemorySystem& memory )
     : m_memory( memory )
@@ -48,21 +48,19 @@ bool Sound::init()
         return false;
     }
 
-    m_stream = SDL_NewAudioStream( obtained.format, obtained.channels, obtained.samples, desired.format, desired.channels, desired.samples );
-    if( m_stream == nullptr )
-    {
-        LOG_E( LogCat::SOUND, "Failed creating audio stream [%s]", SDL_GetError() );
-        ERROR_MSG( "Failed creating audio stream [%s]", SDL_GetError() );
-        return false;
-    }
+    //m_stream = SDL_NewAudioStream( obtained.format, obtained.channels, obtained.samples, desired.format, desired.channels, desired.samples );
+    //if( m_stream == nullptr )
+    //{
+    //    LOG_E( LogCat::SOUND, "Failed creating audio stream [%s]", SDL_GetError() );
+    //    ERROR_MSG( "Failed creating audio stream [%s]", SDL_GetError() );
+    //    return false;
+    //}
 
     SDL_PauseAudioDevice( m_device, 0 ); // This means unpause!
 
     m_sound_buffer = new sample_t[AUDIO_BUFFER_SIZE];
     m_square1_channel = new SquareChannel( MemorySpan( m_memory, CH1_SWEEP_ADDR, 5 ) );
     m_square2_channel = new SquareChannel( MemorySpan( m_memory, 0xFF15, 5 ) );
-
-    m_ch1_ms = new MemorySpan( m_memory, CH1_SWEEP_ADDR, 5 );
 
     return true;
 }
@@ -89,9 +87,6 @@ void Sound::deinit()
     
     if( m_device != 0 )
         SDL_CloseAudioDevice( m_device );
-
-
-    delete m_ch1_ms;
 }
 
 void Sound::update( u32 cycles )
@@ -106,19 +101,37 @@ void Sound::update( u32 cycles )
 
         // update frame sequencer
 
+
         // update mixer (ignoring Vin for now as it was never used).
         ++m_mixer_cycles;
-        if( m_mixer_cycles > 95 )
+        if( m_mixer_cycles > 95 ) // 95 WTF!?
         {
             m_mixer_cycles = 0;
 
             sample_t sample = 0;
-            sample_t const sq1_out = static_cast<sample_t>( m_square1_channel->get_output() ) * 128;
-            SDL_MixAudioFormat( (u8*)&sample, (u8*)&sq1_out, AUDIO_S16SYS, sizeof( sample_t ), SDL_MIX_MAXVOLUME );
-            sample_t const sq2_out = static_cast<sample_t>( m_square2_channel->get_output() ) * 128;
-            SDL_MixAudioFormat( (u8*)&sample, (u8*)&sq2_out, AUDIO_S16SYS, sizeof( sample_t ), SDL_MIX_MAXVOLUME );
+            auto mix_sample = [this, &sample]( SND_OUTPUT_TERMINAL const channel_flag, SquareChannel const* const channel, u8 const so_vol )
+            {
+                static int const SO_VOLUME_RATIO = SDL_MIX_MAXVOLUME / 7;
+                if( is_channel_enabled( channel_flag ) )
+                {
+                    sample_t const channel_out = static_cast<sample_t>( channel->get_output() ) * 128;
+                    SDL_MixAudioFormat( (u8*)&sample, (u8*)&channel_out, AUDIO_S16SYS, sizeof( sample_t ), so_vol * SO_VOLUME_RATIO );
+                }
+            };
+
+            u8 const so1_volume = m_memory.read_8( SND_CHANNEL_CONTROL_ADDR ) & SO1_VOLUME;
+
+            mix_sample( SO1_CH1, m_square1_channel, so1_volume );
+            mix_sample( SO1_CH2, m_square2_channel, so1_volume );
 
             m_sound_buffer[m_buff_write_pos++] = sample;
+
+            sample = 0;
+            u8 const so2_volume = ( m_memory.read_8( SND_CHANNEL_CONTROL_ADDR ) & SO2_VOLUME ) >> 4;
+
+            mix_sample( SO2_CH1, m_square1_channel, so2_volume );
+            mix_sample( SO2_CH2, m_square2_channel, so2_volume );
+
             m_sound_buffer[m_buff_write_pos++] = sample;
 
             //m_sound_buffer[m_buff_write_pos++] = static_cast<sample_t>( rand() );
@@ -159,25 +172,30 @@ bool Sound::is_enabled() const
     return ( m_memory.read_8( SND_ON_OFF_ADDR ) & MASTER_SWITCH ) != 0;
 }
 
-void Sound::fill_audio_buffer( Uint8* stream, int len )
+bool Sound::is_channel_enabled( SND_OUTPUT_TERMINAL const channel_flag ) const
 {
-    static sample_t buf[AUDIO_BUFFER_SIZE];
-
-    ASSERT( AUDIO_BUFFER_SIZE * sizeof( sample_t ) >= len );
-
-    auto const bytes_in_stream = SDL_AudioStreamAvailable( m_stream );
-    auto bytes_to_read_from_stream = ( bytes_in_stream < len ) ? bytes_in_stream : len;
-    if( bytes_in_stream > 0 )
-    {
-        if( SDL_AudioStreamGet( m_stream, buf, bytes_to_read_from_stream ) < 0 )
-        {
-            LOG_E( LogCat::SOUND, "Failed to get [%d] samples from audio stream [%s]", bytes_to_read_from_stream, SDL_GetError() );
-            bytes_to_read_from_stream = 0;
-        }
-    }
-
-    size_t const silence_padding = static_cast<size_t>( len ) - static_cast<size_t>( bytes_to_read_from_stream );
-    memset( buf + ( bytes_to_read_from_stream / sizeof(sample_t) ), 0, silence_padding );
-
-    memcpy( stream, buf, len );
+    return ( m_memory.read_8( SND_OUTPUT_TERMINAL_ADDR ) & channel_flag ) != 0;
 }
+
+//void Sound::fill_audio_buffer( Uint8* stream, int len )
+//{
+//    static sample_t buf[AUDIO_BUFFER_SIZE];
+//
+//    ASSERT( AUDIO_BUFFER_SIZE * sizeof( sample_t ) >= len );
+//
+//    auto const bytes_in_stream = SDL_AudioStreamAvailable( m_stream );
+//    auto bytes_to_read_from_stream = ( bytes_in_stream < len ) ? bytes_in_stream : len;
+//    if( bytes_in_stream > 0 )
+//    {
+//        if( SDL_AudioStreamGet( m_stream, buf, bytes_to_read_from_stream ) < 0 )
+//        {
+//            LOG_E( LogCat::SOUND, "Failed to get [%d] samples from audio stream [%s]", bytes_to_read_from_stream, SDL_GetError() );
+//            bytes_to_read_from_stream = 0;
+//        }
+//    }
+//
+//    size_t const silence_padding = static_cast<size_t>( len ) - static_cast<size_t>( bytes_to_read_from_stream );
+//    memset( buf + ( bytes_to_read_from_stream / sizeof(sample_t) ), 0, silence_padding );
+//
+//    memcpy( stream, buf, len );
+//}
--- GBMO/src/sound/sound.h
+++ GBMO/src/sound/sound.h
@@ -6,7 +6,6 @@
 
 class MemorySystem;
 class SquareChannel;
-class MemorySpan;
 
 class Sound
 {
@@ -23,6 +22,27 @@ public:
         IS_CH1_ENABLED = 1,      // Bit 0 - Sound 1 ON flag( Read Only )
     };
 
+    enum SND_OUTPUT_TERMINAL : u8
+    {
+        SO2_CH4 = 1 << 7, // Bit 7 - Output sound 4 to SO2 terminal
+        SO2_CH3 = 1 << 6, // Bit 6 - Output sound 3 to SO2 terminal
+        SO2_CH2 = 1 << 5, // Bit 5 - Output sound 2 to SO2 terminal
+        SO2_CH1 = 1 << 4, // Bit 4 - Output sound 1 to SO2 terminal
+        SO1_CH4 = 1 << 3, // Bit 3 - Output sound 4 to SO1 terminal
+        SO1_CH3 = 1 << 2, // Bit 2 - Output sound 3 to SO1 terminal
+        SO1_CH2 = 1 << 1, // Bit 1 - Output sound 2 to SO1 terminal
+        SO1_CH1 = 1, // Bit 0 - Output sound 1 to SO1 terminal
+    };
+
+
+    enum SND_CONTROL : u8
+    {
+        VIN_TO_SO2 = 1 << 7,    // Bit 7 - Output Vin to SO2 terminal( 1 = Enable )
+        SO2_VOLUME = 0x70,      // Bit 6 - 4 - SO2 output level( volume )  ( 0 - 7 )
+        VIN_TO_SO1 = 1 << 3,    // Bit 3 - Output Vin to SO1 terminal( 1 = Enable )
+        SO1_VOLUME = 0x07,      // Bit 2 - 0 - SO1 output level( volume )  ( 0 - 7 )
+    };
+
     Sound( MemorySystem& memory );
 
     bool init();
@@ -33,10 +53,12 @@ public:
     void disable();
     void enable();
     bool is_enabled() const;
+    bool is_channel_enabled( SND_OUTPUT_TERMINAL const channel_flag ) const;
 
-    void fill_audio_buffer( Uint8* stream, int len );
+    //void fill_audio_buffer( Uint8* stream, int len );
 
 private:
+
     SquareChannel* m_square1_channel;
     SquareChannel* m_square2_channel;
     //SquareOsc m_square1_channel;
@@ -46,7 +68,6 @@ private:
     //SilenceOsc m_silence_channel;
 
     MemorySystem& m_memory;
-    MemorySpan* m_ch1_ms;
 
     u32 m_sound_cycles;
     u32 m_buff_write_pos;
